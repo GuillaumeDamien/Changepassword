f387a963e694802b58879ffb77749980f796874b
GuillaumeDamien <67358071+GuillaumeDamien@users.noreply.github.com>
2023-11-20 11:55:45 +0100

    Add files via upload

---
+++ change_mdp.ps1
@@ -0,0 +1,13 @@
+﻿$user = 'root'
+$pswd = 'toto123'
+$newPswd = 'Lapin12'
+$esx = get-vmhost -name MonESX.local.fr
+$srv = Connect-VIServer -Server $esx.Name -User $user -Password $pswd 4> $null
+   if($srv){
+        Set-VMHostAccount -UserAccount $user -Password $newPswd -Server $srv -Confirm:$false
+        Disconnect-VIServer -Server $esx.Name -Confirm:$false
+                }
+
+    else    {
+Write-Host "Logon failed $($esx.Name)"
+                }
